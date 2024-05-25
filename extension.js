93b7fab842802e83574f2e0d9e8bad30fac9405f
momoca <momoca@yeah.net>
2024-05-25 18:42:01 +0800

    feat: 添加保留原剪切版内容的功能

--- extension.js
+++ extension.js
@@ -1,21 +1,30 @@
 const vscode = require('vscode');
-
-function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.repeatSelection', function () {
-        let editor = vscode.window.activeTextEditor;
-        if (editor) {
-            let selection = editor.selection;
-            let text = editor.document.getText(selection);
-            vscode.commands.executeCommand('editor.action.clipboardCopyAction').then(() => {
-                vscode.commands.executeCommand('editor.action.clipboardPasteAction').then(() => {
-                    editor.edit(editBuilder => {
-                        editBuilder.insert(selection.end, text);
-                    });
+async function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    'extension.repeatSelection',
+    async function () {
+      let editor = vscode.window.activeTextEditor;
+      if (editor) {
+        let selection = editor.selection;
+        let text = editor.document.getText(selection);
+        // 保存剪贴板原来的内容
+        let originalClipboardText = await vscode.env.clipboard.readText();
+        vscode.commands
+          .executeCommand('editor.action.clipboardCopyAction')
+          .then(() => {
+            vscode.commands
+              .executeCommand('editor.action.clipboardPasteAction')
+              .then(() => {
+                editor.edit(async (editBuilder) => {
+                  editBuilder.insert(selection.end, text);
+                  // 恢复剪贴板原来的内容
+                  await vscode.env.clipboard.writeText(originalClipboardText);
                 });
-            });
-        }
-    });
-
-    context.subscriptions.push(disposable);
+              });
+          });
+      }
+    },
+  );
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
